29e149caba40b1981d9fd53098d1021272296183
Kateryna Shepetska <shepetskayaa@gmail.com>
2024-06-25 11:47:14 +0300

    contact form quick fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -21,7 +21,10 @@ const schema: ZodType<FormData> = z.object({
   fullName: z.string().nonempty({ message: 'Full name is required' }),
   businessName: z.string().nonempty({ message: 'Business name is required' }),
   email: z.string().email({ message: 'Invalid email address' }),
-  phoneNumber: z.string().min(10, { message: 'Phone number should be at least 10 characters' }),
+  phoneNumber: z
+    .string()
+    .regex(/^\+[0-9]+$/, { message: 'Phone number must start with + and contain only numbers' })
+    .min(11, { message: 'Phone number should be at least 11 characters' }),
   ordersPerMonth: z.string().nonempty({ message: 'Number of orders is required' }),
 });
 
@@ -68,39 +71,47 @@ const ContactForm: React.FC = () => {
   return (
     <section id="join" className="py-20 bg-white relative">
       <div className="flex flex-col items-center px-4">
-        <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-6 md:mb-7 lg:mb-14 text-center text-accent-color mb-8">Contact <span className="font-extrabold">Us</span></h2>
+        <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight mb-6 md:mb-7 lg:mb-14 text-center text-accent-color mb-8">
+          Contact <span className="font-extrabold">Us</span>
+        </h2>
         <div className="flex flex-col lg:flex-row w-full lg:justify-center">
           <div className="lg:w-1/2 items-center self-center lg:self-start flex flex-col justify-center">
-            <p className="text-center font-bold text-text-color font-raleway text-xs md:text-lg lg:text-xl lg:text-left max-w-xs md:max-w-sm lg:max-w-md mb-8 lg:mt-4 h-full flex items-center">We&lsquo;re here to make &apos;ship&apos; happen seamlessly. Led by the remarkable Filbert Wingrove, our team turns shipping worries into a breeze.</p>
+            <p className="text-center font-bold text-text-color font-raleway text-xs md:text-lg lg:text-xl lg:text-left max-w-xs md:max-w-sm lg:max-w-md mb-8 lg:mt-4 h-full flex items-center">
+              We&lsquo;re here to make &apos;ship&apos; happen seamlessly. Led by the remarkable Filbert Wingrove, our team turns shipping worries into a breeze.
+            </p>
           </div>
           <div className="lg:w-1/2">
             <form className="lg:max-w-lg w-full" onSubmit={handleSubmit(onSubmit)}>
               {formInputs.map((input, index) => (
                 <div key={index} className="mb-4">
-                  <label className="block font-raleway text-xs md:text-xl text-left text-text-color">{input.label}</label>
+                  <label className="block font-raleway text-xs md:text-xl text-left text-text-color">
+                    {input.label}
+                  </label>
                   <input
                     type={input.type}
                     {...register(input.name)}
                     className="w-full px-4 py-2 border rounded-lg transition duration-300 ease-in-out hover:border-blue-500 hover:shadow-md focus:outline-none focus:border-blue-500 focus:shadow-md"
                   />
-                  {errors[input.name as keyof FormData] && (
-                    <span className="text-red-500">{errors[input.name as keyof FormData]?.message}</span>
+                  {errors[input.name] && (
+                    <span className="text-red-500">{errors[input.name]?.message}</span>
                   )}
                 </div>
               ))}
-              <button type="submit" className="w-full bg-accent-color text-white font-bold py-2 px-4 rounded-lg">Submit</button>
+              <button type="submit" className="w-full bg-accent-color text-white font-bold py-2 px-4 rounded-lg">
+                Submit
+              </button>
             </form>
           </div>
         </div>
       </div>
       <div className="hidden w-[40%] lg:block lg:absolute lg:bottom-0 lg:left-0 lg:mr-8">
-      <Image
-        src={Flamingo.src}
-        alt="Flamingo"
-        width={500}
-        height={300}
-        className="w-full h-auto"
-      />
+        <Image
+          src={Flamingo.src}
+          alt="Flamingo"
+          width={500}
+          height={300}
+          className="w-full h-auto"
+        />
       </div>
       <Modal isOpen={isModalOpen} onClose={closeModal} />
     </section>
